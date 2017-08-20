70453b4fb337a0aba90789d3fb317a442fef4249
Juha Paananen <juha.paananen@gmail.com>
2017-08-20 10:53:48 +0300

    Improve test

--- src/test/scala/mojave/LensTest.scala
+++ src/test/scala/mojave/LensTest.scala
@@ -7,10 +7,23 @@ class LensTest extends FreeSpec with Matchers {
   "Lens" - {
     val document = Html(Body("content"))
 
-    "field access" in {
-      val contentLens = lens[Html].field[Tag]("content")
-      contentLens.get(document) should equal(Body("content"))
-      contentLens.set(document)(Head("hello")) should equal(Html(Head("hello")))
+    "partially unsafe field access" - {
+      "view case class through a trait" in {
+        val contentLens = lens[Html].field[Tag]("content")
+        contentLens.get(document) should equal(Body("content"))
+        contentLens.set(document)(Head("hello")) should equal(Html(Head("hello")))
+      }
+
+      "when copy method is missing" - {
+        case class Decoy() { def name = "decoy" }
+        "read access still works" in {
+          lens[Decoy].field[String]("name").get(Decoy()) should equal ("decoy")
+        }
+        "write access fails gracefully" in {
+          assertThrows[NoSuchMethodException](lens[Decoy].field[String]("name").set(Decoy())("new name"))
+        }
+      }
+
     }
 
     "partial lens field access" in {
